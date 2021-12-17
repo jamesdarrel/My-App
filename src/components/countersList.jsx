ee0f4a19a95cef04d15a99bb8a29399fbfb3053d
Mykhaylo Misyuk <jamesdarrel@MacBook-Air-Mykhaylo.local>
2021-12-17 16:31:07 +0200

    added My-App

---
+++ src/components/countersList.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import Counter from "./counter";
+
+const CountersList = () => {
+  const initialState = [
+    { id: 0, value: 0, name: "Ненужная вещь" },
+    { id: 1, value: 4, name: "Ложка" },
+    { id: 2, value: 0, name: "Вилка" },
+    { id: 3, value: 0, name: "Тарелка" },
+    { id: 4, value: 0, name: "Набор минималиста" },
+  ];
+
+  const [counters, setCounters] = useState(initialState);
+
+  const handleIncrement = (id) => {
+    const newInitialState = [...counters];
+    newInitialState.map((element) => {
+      if (element.id === id) {
+        element.value = element.value + 1;
+      }
+    });
+    setCounters(newInitialState);
+  };
+  const handleDecrement = (id) => {
+    const newInitialState = [...counters];
+    newInitialState.map((element) => {
+      if (element.id === id) {
+        element.value = element.value - 1;
+      }
+    });
+    setCounters(newInitialState);
+  };
+  const handleDelete = (id) => {
+    const newCounters = counters.filter((c) => c.id !== id);
+    setCounters(newCounters);
+  };
+  const handleReset = () => {
+    setCounters(initialState);
+    console.log("handleReset");
+  };
+
+  return (
+    <>
+      {counters.map((count) => (
+        <Counter
+          key={count.id}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
+        />
+      ))}
+      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>
+        Сброс
+      </button>
+    </>
+  );
+};
+
+export default CountersList;
